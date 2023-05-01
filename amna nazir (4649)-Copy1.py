3c087043c265e00aff3a7cca8888bed52e57759c
Amnanazir77 <132253780+Amnanazir77@users.noreply.github.com>
2023-05-01 17:45:20 +0500

    Add files via upload

---
+++ amna nazir (4649)-Copy1.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[3]:
+
+
+pd.read_csv("C:\\Users\\hp\\Downloads\\iris.csv")
+
+
+# In[4]:
+
+
+iris=pd.read_csv("C:\\Users\\hp\\Downloads\\iris.csv")
+
+
+# In[5]:
+
+
+iris.plot()
+
+
+# In[ ]:
+
+
+
+
